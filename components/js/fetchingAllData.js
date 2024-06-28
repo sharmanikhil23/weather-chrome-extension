7ccd14efff4e4f6deea00a71e200f801a99271d9
Nikhil Sharma <nikhil23062000@icloud.com>
2024-06-28 08:11:32 -0400

    updated failing backend service

--- components/js/fetchingAllData.js
+++ components/js/fetchingAllData.js
@@ -1,30 +1,32 @@
 const getData = (location, callback) => {
-  let url = `https://gleaming-lion-wetsuit.cyclic.app/?location=${location}`;
-  fetch(url, {
-    method: "GET",
-    // mode: "cors",
-    headers: { "Content-Type": "application/json" },
-  })
-    .then((result) => {
-      if (result.status != 200) {
+  let url = `https://weather-app-backend-2-0.onrender.com/?location=${encodeURIComponent(
+    location
+  )}`;
+
+  fetch(url)
+    .then((response) => {
+      if (response.status !== 200) {
         return callback({ Error: "Error" }, undefined);
       }
-      return result.text();
+      return response.json(); // Assuming the response is JSON
     })
     .then((data) => {
-      data = JSON.parse(data);
+      data = data.data;
+
       let complete = {
         location: data.location,
         data: data,
         hour: new Date().getHours(),
         day: new Date().getDate(),
       };
-
       complete = JSON.stringify(complete);
-
       localStorage.setItem("weather-forcast-from-weather-app", complete);
       complete = JSON.parse(complete);
+      console.log(complete);
       callback(undefined, complete);
+    })
+    .catch((error) => {
+      callback("Error fetching data:", error);
     });
 };
 
